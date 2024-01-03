61163ce9b947c5ab2d834a768ed2568e69fcc71d
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-01-03 13:51:21 +0900

    Time: 23 ms (6.81%), Space: 54.6 MB (7.62%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            left[i] = nums[i];
+            right[n - 1 - i] = nums[n - 1 - i];
+            if (i > 0) {
+                left[i] *= left[i - 1];
+                right[n - 1 - i] *= right[n - i];
+            }
+        }
+
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = 1;
+            if (i > 0)
+                answer[i] *= left[i - 1];
+            if (i < n - 1)
+                answer[i] *= right[i + 1];
+        }
+        return answer;
+    }
+}
