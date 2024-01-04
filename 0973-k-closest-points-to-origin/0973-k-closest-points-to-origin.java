0eed73c1a568a6f101931d8d731a97764983c08e
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-01-04 13:31:45 +0900

    Time: 43 ms (29.54%), Space: 53.9 MB (30.45%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,20 +1,38 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 import static java.lang.Math.*;
 
 class Solution {
-    public double calcDist(int[] point) {
-        return sqrt(pow(point[0], 2) + pow(point[1], 2));
+    public static class Point {
+        double distance;
+        int[] point;
+
+        public Point(double distance, int[] point) {
+            this.distance = distance;
+            this.point = point;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+
+        public int[] getPoint() {
+            return point;
+        }
     }
+    
     public int[][] kClosest(int[][] points, int k) {
         int[][] result = new int[k][2];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> calcDist(a) < calcDist(b) ? -1 : 1);
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparing(a -> a.getDistance()));
+        
         for (int[] point : points) {
-            pq.offer(point);
+            double dist = sqrt(pow(point[0], 2) + pow(point[1], 2));
+            pq.offer(new Point(dist, point));
         }
 
         for (int i = 0; i < k; i++) {
-            result[i] = pq.poll();
+            result[i] = pq.poll().getPoint();
         }
         return result;
     }
