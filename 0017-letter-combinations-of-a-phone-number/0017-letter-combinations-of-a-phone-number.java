dc2855cf85ba943a116a1ac2f5e60ad4e38c78fe
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-01-08 17:21:32 +0900

    Time: 4 ms (42.3%), Space: 42.7 MB (5.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -4,14 +4,11 @@ import java.util.List;
 import java.util.Map;
 
 class Solution {
-    Map<Character, char[]> keyMap;
-    List<String> result;
-
     public List<String> letterCombinations(String digits) {
         if (digits.equals("")) {
             return new ArrayList<>();
         }
-        keyMap = new HashMap<>();
+        Map<Character, char[]> keyMap = new HashMap<>();
         keyMap.put('2', new char[]{'a', 'b', 'c'});
         keyMap.put('3', new char[]{'d', 'e', 'f'});
         keyMap.put('4', new char[]{'g', 'h', 'i'});
@@ -20,20 +17,20 @@ class Solution {
         keyMap.put('7', new char[]{'p', 'q', 'r', 's'});
         keyMap.put('8', new char[]{'t', 'u', 'v'});
         keyMap.put('9', new char[]{'w', 'x', 'y', 'z'});
-
-        result = new ArrayList<>();
-        dfs(0, "", digits);
+        
+        List<String> result = new ArrayList<>();
+        dfs(0, "", digits, keyMap, result);
         return result;
     }
 
-    public void dfs(int index, String path, String digits) {
+    public void dfs(int index, String path, String digits, Map<Character, char[]> keyMap, List<String> result) {
         if (index == digits.length()) {
             result.add(path);
             return;
         }
 
         for (char c : keyMap.get(digits.charAt(index))) {
-            dfs(index + 1, path + c, digits);
+            dfs(index + 1, path + c, digits, keyMap, result);
         }
     }
 }
