0be4da847b8d10d46d86cd30cb4ac398ef694a5d
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-01-27 16:55:06 +0900

    Time: 463 ms (5.66%), Space: 47.2 MB (7.58%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.java
@@ -0,0 +1,29 @@
+class Solution {
+    int longestPath = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return longestPath;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return -1;
+        }
+
+        int left = dfs(node.left) + 1;
+        int right = dfs(node.right) + 1;
+        int leftDist = 0;
+        int rightDist = 0;
+
+        if (node.left != null && node.val == node.left.val) {
+            leftDist = left;
+        }
+        if (node.right != null && node.val == node.right.val) {
+            rightDist = right ;
+        }
+        System.out.println(node.val + ": " + leftDist + ", " + rightDist);
+        longestPath = Math.max(longestPath, leftDist + rightDist);
+        return Math.max(leftDist, rightDist);
+    }
+}
