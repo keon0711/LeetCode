28a28970ff5571220ed2bebe9329821f3026bc3d
Keon Kim <110909423+keon0711@users.noreply.github.com>
2024-05-05 18:25:03 +0900

    Time: 2 ms (92.71%), Space: 46.9 MB (59.29%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,12 +1,21 @@
 class Solution {
+
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (target - numbers[i] == numbers[j]) {
-                    return new int[]{i + 1, j + 1};
-                }
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                right--;
             }
         }
+
         return new int[2];
     }
 }
